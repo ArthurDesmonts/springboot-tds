084be88873106ffb74ed2dbb0c538ee9158c29f1
ArthurCoding <arthur.desmonts@gmail.com>
2023-01-27 11:35:37 +0100

    ajout de la fonctionnalité "ajout d'item"

--- td1/src/main/kotlin/edu/spring/td1/controller/ItemController.kt
+++ td1/src/main/kotlin/edu/spring/td1/controller/ItemController.kt
@@ -3,9 +3,9 @@ package edu.spring.td1.controller
 import edu.spring.td1.models.Item
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.servlet.view.RedirectView
 
 
 @Controller
@@ -27,4 +27,18 @@ class ItemController {
             return elements
         }
 
+    @GetMapping("/items/new")
+    fun newAction() : String {
+        return "newForm"
+    }
+
+    @PostMapping("/addNew")
+    fun addNew(@ModelAttribute item : Item,
+               @SessionAttribute("items") items:HashSet<Item>,
+               attrs:RedirectAttributes)
+        : RedirectView {
+        items.add(item)
+        attrs.addFlashAttribute("msg", "${item.nom} ajouté dans les items")
+        return RedirectView("/")
+    }
 }
--- td1/src/main/kotlin/models/Item.kt
+++ td1/src/main/kotlin/models/Item.kt
@@ -3,7 +3,7 @@ package edu.spring.td1.models
 import org.springframework.web.bind.annotation.ModelAttribute
 
 
-class Item constructor(var nom : String = ""){
+class Item constructor(var nom : String?){
 
     var evaluation : Int = 0
 
