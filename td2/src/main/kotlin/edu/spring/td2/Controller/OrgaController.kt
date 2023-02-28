ab6ec60ab5fdef3f88533324b66c170564767ebd
ArthurCoding <119744690+ArthurCoding@users.noreply.github.com>
2023-02-28 23:24:43 +0100

    locate the bug for update_form, still not fixed

--- td2/src/main/kotlin/edu/spring/td2/Controller/OrgaController.kt
+++ td2/src/main/kotlin/edu/spring/td2/Controller/OrgaController.kt
@@ -59,4 +59,25 @@ class OrgaController {
         }
         return RedirectView("/")
     }
+
+    @GetMapping("/update/{id}")
+    fun updateAction(@PathVariable id:Int, model: ModelMap): String{
+        val option = orgaRepository.findById(id)
+        if(option.isPresent) {
+            model["orga"] = option.get()
+            return "/main/orgas/update_form"
+        }
+        throw ElementNotFoundException("Organisation with id $id not found")
+    }
+    @PostMapping("updateAction")
+    fun submitUpdateAction(orga:Organisation): RedirectView {
+        orgaRepository.save(orga)
+        return RedirectView("/")
+    }
+
+    @GetMapping("display")
+    fun displayAllAction(model: ModelMap): String {
+        model["orgas"] = orgaRepository.findAll()
+        return "/main/orgas/display_all"
+    }
 }
