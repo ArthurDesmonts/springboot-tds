d403e46ac45ed1c2818919b1bdeaa51b0b44c210
ArthurCoding <119744690+ArthurCoding@users.noreply.github.com>
2023-03-02 15:48:04 +0100

    Add developer working

--- src/main/kotlin/edu/spring/stories/controllers/MainController.kt
+++ src/main/kotlin/edu/spring/stories/controllers/MainController.kt
@@ -1,14 +1,30 @@
 package edu.spring.stories.controllers
 
+import edu.spring.stories.entities.Developer
+import edu.spring.stories.repositories.DeveloperRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.view.RedirectView
 
 
 @Controller
+@RequestMapping("/")
 class MainController {
 
-    @GetMapping("/")
-    fun index(): String {
-        return "index"
+    @Autowired
+    lateinit var developerRepository: DeveloperRepository
+    @RequestMapping(path=["","index"])
+    fun indexAction():String{
+        return "main/index"
+    }
+
+    @PostMapping("developer/add")
+    fun addDeveloperAction(developer: Developer): RedirectView{
+        developerRepository.save(developer)
+        return RedirectView("/")
     }
 }
