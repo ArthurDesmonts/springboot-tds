9ad8afb37dc08b81d1cc2571d4ec27b3daef7ed2
ArthurCoding <119744690+ArthurCoding@users.noreply.github.com>
2023-03-03 23:03:04 +0100

    display all developers : working

--- src/main/kotlin/edu/spring/stories/controllers/MainController.kt
+++ src/main/kotlin/edu/spring/stories/controllers/MainController.kt
@@ -4,6 +4,7 @@ import edu.spring.stories.entities.Developer
 import edu.spring.stories.repositories.DeveloperRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,8 +18,9 @@ class MainController {
 
     @Autowired
     lateinit var developerRepository: DeveloperRepository
-    @RequestMapping(path=["","index"])
-    fun indexAction():String{
+    @GetMapping("/")
+    fun indexAction( modelMap: ModelMap):String{
+        modelMap["developer"] = developerRepository.findAll()
         return "main/index"
     }
 
@@ -27,6 +29,5 @@ class MainController {
         developerRepository.save(developer)
         return RedirectView("/")
     }
-
 
 }
